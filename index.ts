721f55300c6c4b25e7dc7124a0a7332555683bcb
Gaya Kessler <gaya@theclevernode.com>
2025-02-25 14:34:11 +0100

    fetching image and placing on canvas

--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import * as fs from "node:fs";
 import minimist from 'minimist';
 import { parse } from 'node-html-parser';
 import fetch from 'node-fetch';
-import { createCanvas } from 'canvas';
+import {createCanvas, loadImage, registerFont} from 'canvas';
+import sharp from 'sharp';
+
+registerFont('./JetBrainsMono-Medium.ttf', { family: 'JetBrainsMono' })
 
 interface SiteConfig {
     baseUrl: string;
@@ -28,7 +31,7 @@ function generateImage(config: SiteConfig, path: string) {
     fetch(url)
         .then((res) => res.text())
         .then(parse)
-        .then((root) => {
+        .then(async (root) => {
             const body = root.querySelector(config.selector || 'body');
 
             if (!body) {
@@ -38,7 +41,6 @@ function generateImage(config: SiteConfig, path: string) {
             const title = body.querySelector('h1')?.textContent;
             const firstImage = body.querySelector('img');
 
-            console.log(title, firstImage?.attrs.src);
             const width = 1200;
             const height = 630;
 
@@ -47,6 +49,23 @@ function generateImage(config: SiteConfig, path: string) {
 
             ctx.fillStyle = "#000";
             ctx.fillRect(0, 0, width, height);
+
+            if (firstImage) {
+                const imgBuffer = await (await fetch([config.baseUrl, firstImage.attrs.src].join(''))).arrayBuffer();
+                const resized = await sharp(imgBuffer)
+                    .resize(width, height)
+                    .png()
+                    .toBuffer();
+
+                const img = await loadImage(resized);
+                ctx.drawImage(img, 0, 0);
+            }
+
+            ctx.font = "72px JetBrainsMono";
+            ctx.textAlign = "left";
+            ctx.fillStyle = "#fff";
+
+            ctx.fillText(title || 'No title', 46, 430);
 
             const buffer = canvas.toBuffer("image/jpeg");
             fs.writeFileSync("./.tmp/poster.jpg", buffer);

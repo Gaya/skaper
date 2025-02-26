52b21372a6e38411f4e618108ef90cb17fba92fb
Gaya Kessler <gaya@theclevernode.com>
2025-02-26 11:20:49 +0100

    rename

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import * as fs from "node:fs";
 
 import minimist from 'minimist';
-import { parse } from 'node-html-parser';
+import { parse, HTMLElement } from 'node-html-parser';
 import fetch from 'node-fetch';
-import { createCanvas, loadImage, registerFont, CanvasRenderingContext2D, Image } from 'canvas';
+import { createCanvas, loadImage, registerFont, CanvasRenderingContext2D } from 'canvas';
 import sharp from 'sharp';
 
 registerFont('./JetBrainsMono-Medium.ttf', { family: 'JetBrainsMono' })
@@ -20,7 +20,6 @@ const sites: Record<string, SiteConfig> = {
         baseUrl: 'http://localhost:4000',
         // baseUrl: 'https://sauce.gaya.pizza',
         selector: 'main article',
-        // background: '#fff',
         background: '#C03319',
         logo: './sauce-logo.svg',
     },
@@ -60,104 +59,109 @@ function formatTitle(title: string, maxWidth: number, ctx: CanvasRenderingContex
     }, '');
 }
 
-function generateImage(config: SiteConfig, path: string) {
+export async function generateImage(root: HTMLElement, config: SiteConfig) {
+    const body = root.querySelector(config.selector || 'body');
+
+    if (!body) {
+        throw new Error(`Could not find body on '${config.selector}'`);
+    }
+
+    const title = body.querySelector('h1')?.textContent;
+    const firstImage = body.querySelector('img');
+
+    const width = 1200;
+    const height = 630;
+
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = config.background || "#000";
+    ctx.fillRect(0, 0, width, height);
+
+    if (firstImage) {
+        console.info('Fetching first image');
+        const imgUrl = firstImage.attrs.src.startsWith('http')
+            ? firstImage.attrs.src
+            : [config.baseUrl, firstImage.attrs.src].join('');
+        const imgBuffer = await (await fetch(imgUrl)).arrayBuffer();
+
+        console.info('Resizing image');
+        const resized = await sharp(imgBuffer)
+            .resize(width, height)
+            .png()
+            .toBuffer();
+
+        console.info('Drawing image');
+        const img = await loadImage(resized);
+        ctx.drawImage(img, 0, 0);
+    }
+
+    if (config.logo) {
+        const imgBuffer = fs.readFileSync(config.logo);
+
+        const resized = await sharp(imgBuffer)
+            .resize(firstImage ? 200 : 300, undefined)
+            .toBuffer();
+
+        const img = await loadImage(resized);
+
+        if (firstImage) {
+            ctx.drawImage(img, 46, 46);
+        } else {
+            ctx.drawImage(img, (width / 2) - (img.width / 2), 46);
+        }
+    }
+
+    ctx.font = "72px JetBrainsMono";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+
+    const titlePadding = 46;
+    const maxTitleWidth = width - (titlePadding * 2);
+
+    const formattedTitle = formatTitle(title || 'No title', maxTitleWidth, ctx);
+    const lines = formattedTitle.split('\n');
+    const lineHeight = ctx.measureText('lines').emHeightDescent;
+
+    const top = height - (lineHeight * lines.length) - titlePadding;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const backgroundPadding = titlePadding / 4;
+
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(
+            titlePadding - backgroundPadding,
+            top + (lineHeight * i),
+            ctx.measureText(line).width + (backgroundPadding * 2),
+            lineHeight,
+        );
+    }
+
+    ctx.fillStyle = "#000";
+    ctx.fillText(formattedTitle, titlePadding, top);
+
+    const buffer = canvas.toBuffer("image/jpeg");
+    return sharp(buffer).jpeg({ mozjpeg: true }).toBuffer();
+}
+
+function fetchHTMLFromURL(url: string): Promise<string> {
+    return fetch(url).then((res) => res.text());
+}
+
+export function generateImageFromURL(config: SiteConfig, path: string) {
     const url = [config.baseUrl, path].join('/');
 
     console.info(`Fetching data from ${url}`);
 
-    return fetch(url)
-        .then((res) => res.text())
+    return fetchHTMLFromURL(url)
         .then(parse)
-        .then(async (root) => {
-            const body = root.querySelector(config.selector || 'body');
-
-            if (!body) {
-                throw new Error(`Could not find body on '${config.selector}'`);
-            }
-
-            const title = body.querySelector('h1')?.textContent;
-            const firstImage = body.querySelector('img');
-
-            const width = 1200;
-            const height = 630;
-
-            const canvas = createCanvas(width, height);
-            const ctx = canvas.getContext('2d');
-
-            ctx.fillStyle = config.background || "#000";
-            ctx.fillRect(0, 0, width, height);
-
-            if (firstImage) {
-                console.info('Fetching first image');
-                const imgUrl = firstImage.attrs.src.startsWith('http')
-                    ? firstImage.attrs.src
-                    : [config.baseUrl, firstImage.attrs.src].join('');
-                const imgBuffer = await (await fetch(imgUrl)).arrayBuffer();
-
-                console.info('Resizing image');
-                const resized = await sharp(imgBuffer)
-                    .resize(width, height)
-                    .png()
-                    .toBuffer();
-
-                console.info('Drawing image');
-                const img = await loadImage(resized);
-                ctx.drawImage(img, 0, 0);
-            }
-
-            if (config.logo) {
-                const imgBuffer = fs.readFileSync(config.logo);
-
-                const resized = await sharp(imgBuffer)
-                    .resize(firstImage ? 200 : 300, undefined)
-                    .toBuffer();
-
-                const img = await loadImage(resized);
-
-                if (firstImage) {
-                    ctx.drawImage(img, 46, 46);
-                } else {
-                    ctx.drawImage(img, (width / 2) - (img.width / 2), 46);
-                }
-            }
-
-            ctx.font = "72px JetBrainsMono";
-            ctx.textAlign = "left";
-            ctx.textBaseline = "top";
-
-            const titlePadding = 46;
-            const maxTitleWidth = width - (titlePadding * 2);
-
-            const formattedTitle = formatTitle(title || 'No title', maxTitleWidth, ctx);
-            const lines = formattedTitle.split('\n');
-            const lineHeight = ctx.measureText('lines').emHeightDescent;
-
-            const top = height - (lineHeight * lines.length) - titlePadding;
-
-            for (let i = 0; i < lines.length; i++) {
-                const line = lines[i];
-                const backgroundPadding = titlePadding / 4;
-
-                ctx.fillStyle = "#fff";
-                ctx.fillRect(
-                    titlePadding - backgroundPadding,
-                    top + (lineHeight * i),
-                    ctx.measureText(line).width + (backgroundPadding * 2),
-                    lineHeight,
-                );
-            }
-
-            ctx.fillStyle = "#000";
-            ctx.fillText(formattedTitle, titlePadding, top);
-
-            const buffer = canvas.toBuffer("image/jpeg");
-            return sharp(buffer).jpeg({ mozjpeg: true }).toBuffer();
-        });
+        .then((root) => generateImage(root, config));
 }
 
 if (args.site && args.path && args.out) {
     (async () => {
-        const buffer = await generateImage(sites[args.site], args.path);
+        const buffer = await generateImageFromURL(sites[args.site], args.path);
         fs.writeFileSync(
             args.out,
             buffer,
